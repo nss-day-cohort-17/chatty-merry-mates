6a09bb390c27afaaf82d7187da5842d4a963b788
LucasMcL <LMcLaughlin09@gmail.com>
2016-12-07 20:31:38 -0600

    made delete button and clear messages buttons work

--- chatty.js
+++ chatty.js
@@ -17,22 +17,37 @@ function loadInitialMessages(loadEvt) {
   console.log("Initial messages were loaded")
 }
 
-// What input does this function need?
+// Executed on click of element with class 'delete-btn'
+var clickEvent
 function deleteMessage(clickEvt) {
   console.log("The delete button was pressed")
-  console.log(clickEvt.target.parentNode)
+  var li = clickEvt.target.parentElement
+  li.remove() // removes li that contained delete button
+
+  var ul = document.getElementById('messages-list')
+  // If no more messages, disable clear messages button
+  if(ul.childElementCount === 0) {
+    disableClearMessagesButton()
+  }
 }
 
 
 // Clear messages
 function clearAllMessages() {
+  // This function should disable the button upon executing
   console.log("The clear message button was pressed")
+  var ul = document.getElementById('messages-list')
+  ul.innerHTML = ""
 }
 
 // adds attribute disabled="disabled" to button
 // When there are no list items
-function disableClearButton() {
+function disableClearMessagesButton() {
+  console.log("Clear Messages Button disabled")
+}
 
+function enableClearMessagesButton() {
+  console.log("Clear Messages Button enabled")
 }
 
 // Executes when enter button is pressed in text field
@@ -42,6 +57,8 @@ function addMessage(keyEvt) {
     console.log("Enter was pressed")
     document.getElementById('messages-list').innerHTML +=
     `<li>${keyEvt.target.value}<button class="btn btn-default delete-btn">Delete</button></li>`
+    // Clear input field
+    document.getElementById('inputMessage').value = ""
   }
 }
 
@@ -72,19 +89,22 @@ JSONRequest.send()
 // Adding event listener to body as per helpful hint
 // Listening to dynamically created elements
 document.querySelector("body").addEventListener("click", function(event) {
-  console.log(event);
+  // console.log(event);
 
   // Handle the click event on any DOM element with a certain class
   // Any element with the class "delete-btn" in its class list will call
   // function delete message
   for (var i = 0; i < event.target.classList.length; i++) {
     if (event.target.classList[i] === "delete-btn") {
-      deleteMessage();
+      deleteMessage(event);
     }
   }
 });
 
+// Listens for keypress on text input field
 document.getElementById('inputMessage').addEventListener('keypress', addMessage)
+// Listens for clear messages button press
+document.getElementById('clearButton').addEventListener('click', clearAllMessages)
 
 
 
